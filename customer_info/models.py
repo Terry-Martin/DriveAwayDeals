a97e66f1f0e584ce6e9f181bdfc65b10eba203de
Terry Martin <terry.martin@gmail.com>
2024-11-29 18:53:46 +0000

    Add CustomerDetails class to models. Create database table

--- customer_info/models.py
+++ customer_info/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class CustomerDetails(models.Model):
+    customerID = models.IntegerField(unique=True)
+    slug = models.IntegerField(unique=True)
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
