34e00c5b0de8e75276f5bc5f09447f964699a2d8
Terry Martin <terry.martin@gmail.com>
2024-12-08 19:21:00 +0000

    Add Staff and Transaction models

--- sale/admin.py
+++ sale/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import CustomerDetail
+from .models import CustomerDetail, Transaction, Staff
 
 # Register your models here.
 admin.site.register(CustomerDetail)
+admin.site.register(Transaction)
+admin.site.register(Staff)
--- sale/models.py
+++ sale/models.py
@@ -1,15 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = ((0, "Draft"), (1, "Published"))
-
 # Create your models here.
+class Staff(models.Model):
+    staff_id = models.IntegerField(primary_key=True)
+    hire_date = models.DateTimeField(auto_now_add=True)
+    first_name = models.TextField
+    second_name = models.TextField
+    email = models.EmailField
+    position = models.TextField
+
+class Transaction(models.Model):
+    transaction_number = models.IntegerField(primary_key=True)
+    #customer_ID = models.ForeignKey(
+        #CustomerDetail, on_delete = models.RESTRICT, related_name="customer_id"
+    #)
+    transaction_type = models.TextField
+    car_registration = models.TextField
+    price = models.DecimalField(max_digits= 9, decimal_places=2)
+    date_completed = models.DateTimeField
+    #updated_by =  models.ForeignKey(
+        #Staff, on_delete = models.RESTRICT, related_name="staff_id"
+    #)
+    
 class CustomerDetail(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    content = models.TextField()
+    customer_id = models.IntegerField(primary_key=True)
+    first_name = models.TextField
+    second_name = models.TextField
+    email = models.EmailField
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    #updated_by = models.ForeignKey(
+        #Staff, on_delete = models.RESTRICT, related_name="staff_id"
+    #)
+   # transaction_number = models.ForeignKey(
+      #  Transaction, on_delete = models.RESTRICT, related_name="transaction_number"
+    #)
+
+
+    
